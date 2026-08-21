27894e60dc95170ac0a58c49ea114f97c3454f61
nipul830 <57082785+nipul830@users.noreply.github.com>
2026-08-21 20:47:56 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -6,4 +6,4 @@ const SUPABASE_URL =
   "https://gwvhuegpkziujcyqzcra.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
-  "ef2f1258c4b730b311a84995ac73262fa16af8e56eb9fc492473256b12e31965";
+  "sb_publishable_peDfskbZ_AXq2doOtkrC9Q_F1JSPCW8";
