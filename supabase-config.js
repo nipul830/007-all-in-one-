919199420152c1b6372fcfa0210245e5fae7ce1a
nipul830 <57082785+nipul830@users.noreply.github.com>
2026-08-21 20:45:13 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,9 @@
-window.SUPABASE_CONFIG = {
-  url: "https://gwvhuegpkziujcyqzcra.supabase.co",
-  publishableKey: "sb_publishable_peDfskbZ_AXq2doOtkrC9Q_F1JSPCW8"
-};
+// =====================================================
+// SUPABASE CONFIG
+// =====================================================
+
+const SUPABASE_URL =
+  "https://gwvhuegpkziujcyqzcra.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY =
+  "ef2f1258c4b730b311a84995ac73262fa16af8e56eb9fc492473256b12e31965";
